48a40e6568684756521f7a7f6c879de31b9350f0
mjwtom <mjwtom@gmail.com>
2015-01-18 00:04:14 -0600

    an profile

--- swift/dedupe/DedupeResp.py
+++ swift/dedupe/DedupeResp.py
@@ -3,21 +3,15 @@ TODO: Read the fingerprints from the object file
 according to the fingerprints. Read the corresponding data chunks to construct the data stream
 '''
 
-import os
-import binascii
-from swift.proxy.controllers.base import Controller
-from swift import gettext_ as _
+from swift.proxy.controllers import base
 
-
-class RespBodyIter(Controller):
-    def __init__(self, app, req, obj_ring, account_name, container_name, object_name):
-        Controller.__init__(self, app)
+class RespBodyIter(object):
+    def __init__(self, req, obj_ring, account_name, container_name, object_name):
         self.req = req
         self.obj_ring = obj_ring
         self.account_name = account_name
         self.container_name = container_name
         self.object_name = object_name
-        self.req_environ_path = req.environ['PATH_INFO']
 
         partition = self.obj_ring.get_part(
             self.account_name, self.container_name, self.object_name)
@@ -31,24 +25,22 @@ class RespBodyIter(Controller):
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __next__(self, req):
         if(self.fp_cur >= self.fp_number):
-            self.req.environ['PATH_INFO'] = self.req_environ_path
             raise StopIteration
         else:
-            fingerprint = self.fingerprints[self.fp_cur*16:self.fp_cur*16+16]
-            self.fp_cur += 1
-            str_fingerprint = binascii.hexlify(fingerprint)
-
-        self.req.environ['PATH_INFO'] = os.path.dirname(self.req_environ_path)
-        self.req.environ['PATH_INFO'] = self.req.environ['PATH_INFO'] + '/' + str_fingerprint
+            fingerprint = self.fingerprints[self.fp_number*16:self.fp_number*16+16]
+            str_fingerprint = ''
+            for a in fingerprint:
+                str_fingerprint += hex(ord(a))[2:]
 
         partition = self.obj_ring.get_part(
             self.account_name, self.container_name, str_fingerprint)
         resp = self.GETorHEAD_base(
-            self.req, _('Object'), self.obj_ring, partition,
-            self.req.swift_entity_path)
+            req, _('Object'), self.obj_ring, partition,
+            req.swift_entity_path)
         return resp.body
 
-    def next(self):
-        return self.__next__()
+
+    def next(self, req):
+        return self.next(req)
