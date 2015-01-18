657b59679e66b6092db820acf581a3d57f0a091b
mjwtom <mjwtom@gmail.com>
2015-01-18 00:04:14 -0600

    an profile

--- swift/dedupe/DedupeResp.py
+++ swift/dedupe/DedupeResp.py
@@ -3,68 +3,44 @@ TODO: Read the fingerprints from the object file
 according to the fingerprints. Read the corresponding data chunks to construct the data stream
 '''
 
-import os
-import binascii
-from swift import gettext_ as _
-from swift.common.storage_policy import POLICIES
-from swift.dedupe.dedupe_container import dedupe_container
-
+from swift.proxy.controllers import base
 
 class RespBodyIter(object):
-    def __init__(self, req, controller):
-        self.controller = controller
+    def __init__(self, req, obj_ring, account_name, container_name, object_name):
         self.req = req
-        self.resp = controller.GETorHEAD(req)
-        self.fingerprints = self.resp.body
-        # self.fp_number = len(self.fingerprints) / 16
-        self.fp_number = len(self.fingerprints) / 32
+        self.obj_ring = obj_ring
+        self.account_name = account_name
+        self.container_name = container_name
+        self.object_name = object_name
+
+        partition = self.obj_ring.get_part(
+            self.account_name, self.container_name, self.object_name)
+        resp = self.GETorHEAD_base(
+            req, _('Object'), self.obj_ring, partition,
+            req.swift_entity_path)
+        self.fingerprints = resp.body
+        self.fp_number = len(self.fingerprints) / 16
         self.fp_cur = 0
-        self.fp_size = 32
-        self.req_environ_path = self.req.environ['PATH_INFO']
 
     def __iter__(self):
         return self
 
-    def __next_chunk__(self):
+    def __next__(self, req):
         if(self.fp_cur >= self.fp_number):
-            self.req.environ['PATH_INFO'] = self.req_environ_path
             raise StopIteration
         else:
-            fingerprint = self.fingerprints[self.fp_cur*self.fp_size:self.fp_cur*self.fp_size+self.fp_size]
-            self.fp_cur += 1
+            fingerprint = self.fingerprints[self.fp_number*16:self.fp_number*16+16]
+            str_fingerprint = ''
+            for a in fingerprint:
+                str_fingerprint += hex(ord(a))[2:]
 
-        self.req.environ['PATH_INFO'] = os.path.dirname(self.req_environ_path)
-        self.req.environ['PATH_INFO'] = self.req.environ['PATH_INFO'] + '/' + fingerprint
-
-        self.controller.object_name = fingerprint
-        resp = self.controller.GETorHEAD(self.req)
+        partition = self.obj_ring.get_part(
+            self.account_name, self.container_name, str_fingerprint)
+        resp = self.GETorHEAD_base(
+            req, _('Object'), self.obj_ring, partition,
+            req.swift_entity_path)
         return resp.body
 
-    def __next__(self):
-        if(self.fp_cur >= self.fp_number):
-            self.req.environ['PATH_INFO'] = self.req_environ_path
-            raise StopIteration
-        else:
-            fingerprint = self.fingerprints[self.fp_cur*self.fp_size:self.fp_cur*self.fp_size+self.fp_size]
-            self.fp_cur += 1
-        dedupe = self.controller.dedupe
-        container_id = dedupe.lookup(fingerprint)
 
-        if container_id == str(dedupe.container_count):
-            return dedupe.container.kv[fingerprint]
-
-        self.req.environ['PATH_INFO'] = os.path.dirname(self.req_environ_path)
-        self.req.environ['PATH_INFO'] = self.req.environ['PATH_INFO'] + '/' + str(container_id)
-
-        container = dedupe_container(container_id)
-
-        self.controller.object_name = container_id
-        resp = self.controller.GETorHEAD(self.req)
-        container.frombyte(resp.body)
-        return container.kv[fingerprint]
-
-    def next(self):
-        return self.__next__()
-
-    def get_resp(self):
-        return self.resp
+    def next(self, req):
+        return self.next(req)
