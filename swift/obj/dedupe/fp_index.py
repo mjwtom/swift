e29d871259352c171f40de005044da1deaf6ddf9
mjwtom <mjwtom@gmail.com>
2015-08-11 03:36:25 -0500

    add commit to SQLite insert function. But insert becomes very slow

--- swift/obj/dedupe/fp_index.py
+++ swift/obj/dedupe/fp_index.py
@@ -18,32 +18,33 @@ class fp_index:
     def insert(self, key, value):
         data = (key, value)
         self.c.execute('INSERT INTO fp_index VALUES (?, ?)', data)
+        self.conn.commit()
 
     def lookup(self, key):
         data = (key,)
         self.c.execute('SELECT value FROM fp_index WHERE key=?', data)
-        return self.c
+        return self.c.fetchone()
 
 
 def testinsert():
     fp = fp_index('/home/mjwtom/mydb.db')
-    for i in range(0, 1000):
+    for i in range(0, 100):
         str = i.__str__()
         fp.insert(str, str)
 
 def testselect():
     fp = fp_index('/home/mjwtom/mydb.db')
-    for i in range(0, 1000):
+    for i in range(0, 100):
         str = i.__str__()
         c = fp.lookup(str)
         for row in c:
             print row
 
 
-        '''
+'''
 if __name__ == '__main__':
     unittest.main()
-            '''
+'''
 
 testinsert()
 testselect()
