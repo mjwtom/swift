f86829cb65568b4151f42c0e7e8b9327548a9067
mjwtom <mjwtom@gmail.com>
2015-08-25 18:53:55 -0500

    Use database to detect the duplication. But the md5 value does not match. Need to add some code here

--- swift/dedupe/environment.py
+++ swift/dedupe/environment.py
@@ -22,6 +22,32 @@ RSYNC_ENABLE=true
 
 $PrivDropToGroup adm
 
+5.Edit the $HOME/bin/resetswift script
+
+The template resetswift script looks like the following:
+
+    #!/bin/bash
+
+    swift-init all stop
+    # Remove the following line if you did not set up rsyslog for individual logging:
+    sudo find /var/log/swift -type f -exec rm -f {} \;
+    sudo umount /mnt/sdb1
+    # If you are using a loopback device set SAIO_BLOCK_DEVICE to "/srv/swift-disk"
+    sudo mkfs.xfs -f ${SAIO_BLOCK_DEVICE:-/dev/sdb1}
+    sudo mount /mnt/sdb1
+    sudo mkdir /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4
+    sudo chown ${USER}:${USER} /mnt/sdb1/*
+    mkdir -p /srv/1/node/sdb1 /srv/1/node/sdb5 \
+             /srv/2/node/sdb2 /srv/2/node/sdb6 \
+             /srv/3/node/sdb3 /srv/3/node/sdb7 \
+             /srv/4/node/sdb4 /srv/4/node/sdb8
+    sudo rm -f /var/log/debug /var/log/messages /var/log/rsyncd.log /var/log/syslog
+    find /var/cache/swift* -type f -name *.recon -exec rm -f {} \;
+    # On Fedora use "systemctl restart <service>"
+    sudo service rsyslog restart
+    sudo service memcached restart
+
+
 
 '''
 
@@ -70,7 +96,7 @@ if __name__ == '__main__':
     chown <your-user-name>:<your-group-name> /var/run/swift
     '''
 
-    os.system('cd $HOME/PycharmProjects/swift; sudo pip install -r requirements.txt; sudo python setup.py develop;')
+    os.system('cd $HOME/PycharmProjects/swift; sudo pip install -r requirements.txt; sudo python setup.py develop; cd -')
     os.system('sudo pip install -U xattr')
     os.system('cd $HOME/PycharmProjects/swift; sudo pip install -r test-requirements.txt')
 
@@ -88,6 +114,7 @@ if __name__ == '__main__':
     # do step 4 here
 
     os.system('sudo mkdir -p /var/log/swift/hourly')
+    os.system('sudo mkdir -p /var/log/swift')
 
     os.system('sudo chown -R syslog.adm /var/log/swift')
     os.system('sudo chmod -R g+w /var/log/swift')
@@ -102,6 +129,8 @@ if __name__ == '__main__':
     os.system('mkdir -p $HOME/bin')
     os.system('cd $HOME/PycharmProjects/swift/doc; cp saio/bin/* $HOME/bin; cd -')
     os.system('chmod +x $HOME/bin/*')
+
+    # do step 5 here
 
     os.system('echo "export SAIO_BLOCK_DEVICE=/srv/swift-disk" >> $HOME/.bashrc')
     os.system('sed -i "/find \/var\/log\/swift/d" $HOME/bin/resetswift')
