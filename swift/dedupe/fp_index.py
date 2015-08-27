cfb2ed1955c2dad28e3d4393ccda8814693720f1
mjwtom <mjwtom@gmail.com>
2015-08-27 03:47:19 -0500

    find a cache to use

--- swift/dedupe/fp_index.py
+++ swift/dedupe/fp_index.py
@@ -4,24 +4,35 @@ import sqlite3
 import unittest
 
 
-class Fp_Index(object):
+class fp_index(object):
     def __init__(self, name):
-        if name.endswith('.db'):
+        if name.endswith('.db') or (name == ':memory:'):
             self.name = name
         else:
             self.name = name + '.db'
-        self.conn = sqlite3.connect(name)
+        self.conn = sqlite3.connect(self.name)
         self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXISTS fp_index (key text, value text)''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS fp_index (key text, value text, obj_hash text)''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS obj_fps (obj_hash text, fps)''')
 
 
-    def insert(self, key, value):
-        data = (key, value)
-        self.c.execute('INSERT INTO fp_index VALUES (?, ?)', data)
+    def insert_fp_index(self, key, value, obj_hash):
+        data = (key, value, obj_hash)
+        self.c.execute('INSERT INTO fp_index VALUES (?, ?, ?)', data)
         self.conn.commit()
 
-    def lookup(self, key):
+    def insert_obj_fps(self, obj_hash, fps):
+        data = (obj_hash, fps)
+        self.c.execute('INSERT INTO obj_fps VALUES (?, ?)', data)
+        self.conn.commit()
+
+    def lookup_fp_index(self, key):
         data = (key,)
+        self.c.execute('SELECT value FROM fp_index WHERE key=?', data)
+        return self.c.fetchone()
+
+    def lookup_obj_fps(self, obj_hash):
+        data = (obj_hash,)
         self.c.execute('SELECT value FROM fp_index WHERE key=?', data)
         return self.c.fetchone()
 
