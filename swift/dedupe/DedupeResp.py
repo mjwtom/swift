83eb60d6d857e370123ea66331e0da0281a7aae3
mjwtom <mjwtom@gmail.com>
2015-08-30 03:27:57 -0500

    using the new openstack/swift as the base. Read has problem to be solved

--- swift/dedupe/DedupeResp.py
+++ swift/dedupe/DedupeResp.py
@@ -13,29 +13,12 @@ class RespBodyIter(object):
     def __init__(self, req, controller):
         self.controller = controller
         self.req = req
-        container_info = controller.container_info(
-            controller.account_name, controller.container_name, req)
-        req.acl = container_info['read_acl']
-        # pass the policy index to storage nodes via req header
-        policy_index = req.headers.get('X-Backend-Storage-Policy-Index',
-                                       container_info['storage_policy'])
-        policy = POLICIES.get_by_index(policy_index)
-        obj_ring = controller.app.get_object_ring(policy_index)
-        req.headers['X-Backend-Storage-Policy-Index'] = policy_index
-        if 'swift.authorize' in req.environ:
-            aresp = req.environ['swift.authorize'](req)
-            if aresp:
-                return aresp
-        partition = obj_ring.get_part(
-            controller.account_name, controller.container_name, controller.object_name)
-        node_iter = controller.app.iter_nodes(obj_ring, partition)
         resp = controller.GETorHEAD(req)
         self.fingerprints = resp.body
         # self.fp_number = len(self.fingerprints) / 16
         self.fp_number = len(self.fingerprints) / 32
         self.fp_cur = 0
         self.fp_size = 32
-        self.obj_ring = obj_ring
         self.req_environ_path = self.req.environ['PATH_INFO']
 
     def __iter__(self):
@@ -71,8 +54,7 @@ class RespBodyIter(object):
         self.req.environ['PATH_INFO'] = os.path.dirname(self.req_environ_path)
         self.req.environ['PATH_INFO'] = self.req.environ['PATH_INFO'] + '/' + fingerprint
 
-        partition = self.obj_ring.get_part(
-            self.controller.account_name, self.controller.container_name, fingerprint)
+        self.controller.object_name = fingerprint
         resp = self.controller.GETorHEAD(self.req)
         return resp.body
 
