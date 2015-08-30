2c705c3ea9261b82db7cf1576a61b6711b5402e9
mjwtom <mjwtom@gmail.com>
2015-08-30 03:03:39 -0500

    using the new openstack/swift as the base. Read has problem to be solved

--- swift/dedupe/DedupeResp.py
+++ swift/dedupe/DedupeResp.py
@@ -5,38 +5,48 @@ according to the fingerprints. Read the corresponding data chunks to construct t
 
 import os
 import binascii
-from swift.proxy.controllers.base import Controller
 from swift import gettext_ as _
+from swift.common.storage_policy import POLICIES
 
 
-class RespBodyIter(Controller):
-    def __init__(self, app, req, obj_ring, account_name, container_name, object_name):
-        Controller.__init__(self, app)
+class RespBodyIter(object):
+    def __init__(self, req, controller):
+        self.controller = controller
         self.req = req
-        self.obj_ring = obj_ring
-        self.account_name = account_name
-        self.container_name = container_name
-        self.object_name = object_name
-        self.req_environ_path = req.environ['PATH_INFO']
-
-        partition = self.obj_ring.get_part(
-            self.account_name, self.container_name, self.object_name)
-        resp = self.GETorHEAD_base(
-            req, _('Object'), self.obj_ring, partition,
-            req.swift_entity_path)
+        container_info = controller.container_info(
+            controller.account_name, controller.container_name, req)
+        req.acl = container_info['read_acl']
+        # pass the policy index to storage nodes via req header
+        policy_index = req.headers.get('X-Backend-Storage-Policy-Index',
+                                       container_info['storage_policy'])
+        policy = POLICIES.get_by_index(policy_index)
+        obj_ring = controller.app.get_object_ring(policy_index)
+        req.headers['X-Backend-Storage-Policy-Index'] = policy_index
+        if 'swift.authorize' in req.environ:
+            aresp = req.environ['swift.authorize'](req)
+            if aresp:
+                return aresp
+        partition = obj_ring.get_part(
+            controller.account_name, controller.container_name, controller.object_name)
+        node_iter = controller.app.iter_nodes(obj_ring, partition)
+        resp = controller.GETorHEAD(req)
         self.fingerprints = resp.body
-        self.fp_number = len(self.fingerprints) / 16
+        # self.fp_number = len(self.fingerprints) / 16
+        self.fp_number = len(self.fingerprints) / 32
         self.fp_cur = 0
+        self.fp_size = 32
+        self.obj_ring = obj_ring
+        self.req_environ_path = self.req.environ['PATH_INFO']
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __next_raw__(self):
         if(self.fp_cur >= self.fp_number):
             self.req.environ['PATH_INFO'] = self.req_environ_path
             raise StopIteration
         else:
-            fingerprint = self.fingerprints[self.fp_cur*16:self.fp_cur*16+16]
+            fingerprint = self.fingerprints[self.fp_cur*self.fp_size:self.fp_cur*self.fp_size+self.fp_size]
             self.fp_cur += 1
             str_fingerprint = binascii.hexlify(fingerprint)
 
@@ -48,6 +58,22 @@ class RespBodyIter(Controller):
         resp = self.GETorHEAD_base(
             self.req, _('Object'), self.obj_ring, partition,
             self.req.swift_entity_path)
+        return resp.body
+
+    def __next__(self):
+        if(self.fp_cur >= self.fp_number):
+            self.req.environ['PATH_INFO'] = self.req_environ_path
+            raise StopIteration
+        else:
+            fingerprint = self.fingerprints[self.fp_cur*self.fp_size:self.fp_cur*self.fp_size+self.fp_size]
+            self.fp_cur += 1
+
+        self.req.environ['PATH_INFO'] = os.path.dirname(self.req_environ_path)
+        self.req.environ['PATH_INFO'] = self.req.environ['PATH_INFO'] + '/' + fingerprint
+
+        partition = self.obj_ring.get_part(
+            self.controller.account_name, self.controller.container_name, fingerprint)
+        resp = self.controller.GETorHEAD(self.req)
         return resp.body
 
     def next(self):
